a30b33fed88f40a8fb0f603708dd53edd8c39b7a
phạm tú tài <93572750+phamtutai1338@users.noreply.github.com>
2023-03-21 22:29:03 +0700

    up

---
+++ Project3/Areas/Admin/Models/Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace Project3.Areas.Admin.Models
+{
+    public class Admin
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
